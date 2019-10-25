785970aea713910af5dda10335d87d92989044d9
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-10-25 03:20:13 +0300

    Add player movement and post-processing

---
+++ Assets/Scripts/Controls/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraController : MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float damping = 5;
+
+    private Vector3 offset;
+    #endregion
+
+    private void Awake() {
+        Assert.IsNotNull(target, "target must not be null");
+        offset = target.transform.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = target.transform.position - offset;
+        newPosition.y = transform.position.y;
+
+        transform.position = newPosition;
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const int RESPAWN_Y_THRESHOLD = -80;
+
+    #region Fields
+    [SerializeField, Tooltip("Units per second")]
+    private float speed;
+
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+    #endregion
+
+    private void Awake() {
+        spawnPosition = transform.position;
+        spawnRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        CheckRestart();
+    }
+
+    private void UpdateMovement() {
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(dx, 0, dz).normalized * speed * Time.deltaTime;
+        transform.position += movement;
+
+        if (movement.sqrMagnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+
+    private void CheckRestart()
+    {
+        if (transform.position.y < RESPAWN_Y_THRESHOLD)
+        {
+            transform.rotation = spawnRotation;
+            transform.position = spawnPosition;
+        }
+    }
+}
