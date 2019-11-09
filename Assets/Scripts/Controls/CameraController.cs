4aec2ec356fed52d32b1e553a0e90d1c3074867f
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-11-09 02:20:04 +0200

    Update camera follow script

--- Assets/Scripts/Controls/CameraController.cs
+++ Assets/Scripts/Controls/CameraController.cs
@@ -2,25 +2,49 @@
 using UnityEngine.Assertions;
 
 public class CameraController : MonoBehaviour {
+
     #region Fields
     [SerializeField]
-    private Transform target;
+    private Transform players;
+    [SerializeField, Range(10, 20)]
+    private float zoom = 15;
 
-    [SerializeField]
-    private float damping = 5;
-
-    private Vector3 offset;
+    private Vector3 targetPos;
+    private float startY;
     #endregion
 
     private void Awake() {
-        Assert.IsNotNull(target, "target must not be null");
-        offset = target.transform.position - transform.position;
+        Assert.IsNotNull(players, "target must not be null");
+        startY = transform.position.y;
     }
 
     private void FixedUpdate() {
-        Vector3 newPosition = target.transform.position;
-        newPosition.y = transform.position.y;
+        Recalculate();
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.fixedDeltaTime);
+    }
 
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.fixedDeltaTime);
+    private void Recalculate() {
+        if ((players?.childCount ?? 0) == 0) {
+            targetPos = transform.position;
+            return;
+        }
+
+        targetPos = new Vector3();
+        foreach (Transform player in players) {
+            targetPos += player.transform.position;
+        }
+        targetPos /= players.childCount;
+
+        float maxDistance = 0;
+        foreach (Transform player in players) {
+            Vector3 distance = targetPos - player.position;
+            distance.y = 0;
+            if (distance.magnitude > maxDistance) {
+                maxDistance = distance.magnitude;
+            }
+        }
+
+        float deltaY = maxDistance - zoom;
+        targetPos.y = startY + deltaY;
     }
 }
