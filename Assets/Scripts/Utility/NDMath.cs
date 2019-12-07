4a48b91ed5457e0c7db0ddb349d6686de07f70cb
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-12-07 14:58:27 +0200

    Fix smoothing methods

--- Assets/Scripts/Utility/NDMath.cs
+++ Assets/Scripts/Utility/NDMath.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class NDMath {
 
     public static double SmoothStartN(double t, int n) {
+        if (t == 0) {
+            return 0;
+        }
+
         double res = 1;
         for (int i = 0; i < n; i++) {
             res *= t;
@@ -11,11 +15,15 @@ public class NDMath {
     }
 
     public static double SmoothStopN(double t, int n) {
+        if (t == 0) {
+            return 0;
+        }
+
         double res = 1;
         for (int i = 0; i < n; i++) {
             res *= (1 - t);
         }
-        return res;
+        return 1 - res;
     }
 
     public static Vector3 SmoothStartVectorN(Vector3 a, Vector3 b, double t, int n) {
