a4b8bcc882e9a5909647e378dabf6908a3667f8c
warriormaster12 <streng.alexander@gmail.com>
2019-12-10 18:11:20 +0200

    Finished Main menu layout

--- Assets/Scripts/Callbacks/GotoSceneCallback.cs
+++ Assets/Scripts/Callbacks/GotoSceneCallback.cs
@@ -5,10 +5,19 @@ public class GotoSceneCallback : BehaviourCallback {
     #region Fields
     [SerializeField]
     private string sceneName;
+    [SerializeField]
+    private bool QuitGame = false;
     #endregion
 
     public override void Call() {
-        G.Instance.Round.Reset();
-        G.Instance.Scene.Load(sceneName);
+        if (QuitGame == true)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            G.Instance.Round.Reset();
+            G.Instance.Scene.Load(sceneName);  
+        }  
     }
 }
