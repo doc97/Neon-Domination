08f5848e394590a8cb7e3803ba16513623e4a15d
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-12-11 18:32:42 +0200

    Add exit to main menu script

---
+++ Assets/Scripts/Management/ExitToMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ExitToMenu : MonoBehaviour {
+
+    [SerializeField]
+    private KeyCode key;
+
+    public void Update() {
+        if (Input.GetKeyDown(key)) {
+            G.Instance.Scene.Load("MainMenu");
+        }
+    }
+}
